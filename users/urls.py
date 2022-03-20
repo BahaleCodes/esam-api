0b37dae0e9ee25cae1e7127db699fc09649a5a5e
BahaleCodes <74527555+BahaleCodes@users.noreply.github.com>
2022-03-20 20:49:02 +0200

    Update

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('create/', CustomUserCreate.as_view(), name="create_user"),
     path('profiles/',Profile_list.as_view(), name="get_artists_profiles"),
     path('profile/', UserDetail.as_view(), name='detailArticle'),
-    
-
 ]
